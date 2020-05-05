13b09ff1f3a222e79620071722e0e9974ce705a0
nedimcelik <54439547+abidinkarpuz@users.noreply.github.com>
2020-05-05 18:29:03 +0200

    add secondClass.java

---
+++ src/tests/secondClass.java
@@ -0,0 +1,9 @@
+package tests;
+
+import java.nio.charset.MalformedInputException;
+
+public class secondClass {
+    public static void main(String[] args) {
+        System.out.println("second one");
+    }
+}
