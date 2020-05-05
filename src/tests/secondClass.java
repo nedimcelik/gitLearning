fe57f3e5b98b018706ceb2e0df6e448536e27afd
nedimcelik <54439547+abidinkarpuz@users.noreply.github.com>
2020-05-05 18:35:51 +0200

    update secondClass.java

--- src/tests/secondClass.java
+++ src/tests/secondClass.java
@@ -1,9 +1,8 @@
 package tests;
 
-import java.nio.charset.MalformedInputException;
-
 public class secondClass {
     public static void main(String[] args) {
         System.out.println("second one");
+        System.out.println("second one update");
     }
 }
